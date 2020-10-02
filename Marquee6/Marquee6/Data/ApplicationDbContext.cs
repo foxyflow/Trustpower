563e8e4b96d7903b83e4c177cc736a86b4de3e40
Luke <foxyflow@contractor.net>
2020-10-01 18:26:10 -1200

    Luke's branch read all readmes

---
+++ Marquee6/Marquee6/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Marquee6.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Marquee6.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+         public DbSet<Category> Category { get; set; }
+    }
+}
